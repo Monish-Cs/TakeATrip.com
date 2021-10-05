420d17bfc6dc93a93edc44f3db6e8d163a2c9b4a
Monish <monishsekar03@gmail.com>
2021-10-05 15:49:44 +0530

    Changes in viewsController

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req,res)=>{
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync')
+exports.getOverview = catchAsync(async (req,res,next)=>{
+    //1) Get Tour data from collection
+    const tours = Tour.find();
+    //2) Build Template
+
+    //3) Render that template using tour data from 1)
     res.status(200).render('overview',{
-        title: 'All tours'
+        title: 'All tours',
+        tours
     });
-};
+});
 exports.getTour =  (req,res)=>{
     res.status(200).render('tour',{
         title: 'The Fores Hiker Tour'
