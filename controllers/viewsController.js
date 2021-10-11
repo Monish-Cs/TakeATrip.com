e55ee6ca4669aa083577255ba6f1f4d537146932
Monish <monishsekar03@gmail.com>
2021-10-11 20:59:41 +0530

    Add Tour Template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -11,10 +11,19 @@ exports.getOverview = catchAsync(async (req,res,next)=>{
         tours
     });
 });
-exports.getTour =  (req,res)=>{
+exports.getTour =  catchAsync(async (req,res,next)=>{
     // 1) get the data for the requested tour(including reviews and data)
+    console.log(req.params.slug);
+    const tour=await Tour.findOne({slug: req.params.slug}).populate({
+        path: 'reviews',
+        fields: 'review rating user'
+    });
+    // 2) Build Template
+
+    // 3) Render template using data from 1)
 
     res.status(200).render('tour',{
-        title: 'The Fores Hiker Tour'
+        title: `${tour.name} Tour`,
+        tour
     });
-};
+});
