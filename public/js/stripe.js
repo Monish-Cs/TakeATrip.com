335cb049c9973259a1f86863bf4775456bbc4f74
Monish <monishsekar03@gmail.com>
2021-11-23 05:50:56 +0530

    Prepared to deploy

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,7 +8,7 @@ export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
     const session = await axios(
-      `http://127.0.0.1:8000/api/v1/bookings/checkOut-Session/${tourId}`
+      `/api/v1/bookings/checkOut-Session/${tourId}`
     );
     //console.log(session);
     // 2) Create checkout form + chanre credit card
